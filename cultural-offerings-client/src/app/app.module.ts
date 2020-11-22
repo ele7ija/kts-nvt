ce60df610411015dfd5aa9ae556da41e4dd36996
Ivana Marosevic <i.marosevic@hotmail.com>
2020-11-22 19:59:41 +0100

    Added components homepage and not-found, app routing is set, duplicating imports are organised.

--- cultural-offerings-client/src/app/app.module.ts
+++ cultural-offerings-client/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
