63eef4c2a251f7552181e4d3c284f7b2522ff092
bpoprzen <bpoprzen@gmail.com>
2020-11-24 20:43:24 +0100

    Init Api and Controller classes

---
+++ cultural-offerings/src/main/java/ftn/ktsnvt/culturalofferings/controller/impl/RatingController.java
@@ -0,0 +1,35 @@
+package ftn.ktsnvt.culturalofferings.controller.impl;
+
+import ftn.ktsnvt.culturalofferings.controller.api.RatingApi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class RatingController implements RatingApi {
+
+    private static final Logger log = LoggerFactory.getLogger(RatingController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public RatingController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+
+}
