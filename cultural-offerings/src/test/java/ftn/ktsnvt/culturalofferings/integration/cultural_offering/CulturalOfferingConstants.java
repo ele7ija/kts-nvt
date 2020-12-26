1067e589cc1592efe905f8ac1434c79c7b4223fa
Ivana Marosevic <i.marosevic@hotmail.com>
2020-12-26 12:56:45 +0100

    Finished tests for CulturalOfferingRepository

---
+++ cultural-offerings/src/test/java/ftn/ktsnvt/culturalofferings/integration/cultural_offering/CulturalOfferingConstants.java
@@ -0,0 +1,22 @@
+package ftn.ktsnvt.culturalofferings.integration.cultural_offering;
+
+public class CulturalOfferingConstants {
+
+	static final int LIST_SIZE = 4;
+    static final int PAGE_NUMBER = 0;
+    static final int PAGE_SIZE = 2;
+    
+    static final String EXISTING_ENTITY_NAME = "Spomenik kralju Peri";
+    static final String NON_EXISTING_ENTITY_NAME = "Spomenik kralju Perici";
+    
+    static final long EXISTING_ENTITY_TYPE_ID = 10l;
+    static final long NON_EXISTING_ENTITY_TYPE_ID = 100l;
+    static final long EXISTING_ENTITY_SUBTYPE_ID = 10l;
+    static final long NON_EXISTING_ENTITY_SUBTYPE_ID = 100l;
+    static final int FOUND_ELEMENTS_SUCCESS = 1;
+
+
+
+
+	
+}
