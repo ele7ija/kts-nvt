734df36214179e520d45d08e6682f099f293c578
Ivana Marosevic <i.marosevic@hotmail.com>
2020-12-27 13:28:08 +0100

    Finished LocationService integration tests.

---
+++ cultural-offerings/src/test/java/ftn/ktsnvt/culturalofferings/integration/location/LocationConstants.java
@@ -0,0 +1,15 @@
+package ftn.ktsnvt.culturalofferings.integration.location;
+
+public class LocationConstants {
+	
+    static final long EXISTING_ENTITY_ID = 11l;
+    static final String EXISTING_ENTITY_NAME = "Trg republike BB, Novi Sad";
+    static final long NON_EXISTING_ENTITY_ID = 25l;
+    
+    static final int LIST_SIZE = 3;
+    static final int PAGE_NUMBER = 0;
+    static final int PAGE_SIZE = 2;
+    static final int TOTAL_PAGES = 2;
+    static final int UNREACHABLE_PAGE_NUMBER = 5;
+    
+}
